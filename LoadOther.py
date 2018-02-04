67a299e582d4629dd2f35d0124bf88dc6b9950da
WangGuobao <wangguobaoyfd@163.com>
2018-02-04 11:34:32 +0800

    引入其他文件的类

--- LoadOther.py
+++ LoadOther.py
@@ -1,8 +1,10 @@
 # coding=utf-8
+
+# 引入其他文件的类
 # 第一种引入方法
-import FirstWork
+# import FirstWork
 # 第二种引入方法
-# from FirstWork import SecondTest
+from FirstWork import SecondTest
 
 S=FirstWork.SecondTest("LoadOtherClass")
 S.SayFirst()
